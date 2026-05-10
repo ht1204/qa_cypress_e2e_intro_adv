5e223b944333e9a233be833887f32cd6f3338031
Hector Triana <ptb1204@hotmail.com>
2026-05-10 15:25:00 -0500

    fix: proposal url solve

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://react-redux.realworld.io',
+    baseUrl: 'https://conduit.mate.academy/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
