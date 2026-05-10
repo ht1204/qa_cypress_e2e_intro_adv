fbd213b89527cf01ce15a40b68d49ede78f173c6
Hector Triana <ptb1204@hotmail.com>
2026-05-10 15:18:04 -0500

    fix: change link - R1

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://conduit.mate.academy/',
+    baseUrl: 'https://react-redux.realworld.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
